67d6a0a520382ffea30b5f6225e5ae3653acc8f6
Eric <naibau2004@gamil.com>
2014-09-21 19:39:24 +0800

    Develop 2014/09/21

--- API_Test/checkFile_Test/checkFile_Test.java
+++ API_Test/checkFile_Test/checkFile_Test.java
@@ -9,22 +9,47 @@ public class checkFile_Test
 	{
 		String list_Path = "C:\\Java-Test\\Loop_Test\\loop_list.txt" ;
 		String Single_File = "C:\\Java-Test\\Loop_Test\\1.TXT" ;
-				
+
+		
+//		多檔案測試是否存在
 		checkFile b = new check_file_haveloop_useFile( list_Path ) ;	
-		System.out.println ( b.get_all() ) ;
-		System.out.println ( b.get_boolean() ) ;
 		for ( int i = 0 ; i < b.get_all().size() ; i++ )
 		{
 			System.out.println ( b.get_all().get(i) + " : " +  b.get_boolean().get(i) ) ;
 		}
 		
-			
+		System.out.println ( "-----------------------------" ) ;
+		
+		
+//		僅顯示存在的檔案清單
+		for ( int i = 0 ; i < b.get_exist().size() ; i++ )
+		{
+			System.out.println ( b.get_exist().get(i) ) ;
+		}
+		
+		System.out.println ( "-----------------------------" ) ;	
+		
+	
+//		僅顯示不存在的檔案清單
+		for ( int i = 0 ; i < b.get_noexist().size() ; i++ )
+		{
+			System.out.println ( b.get_noexist().get(i) ) ;
+		}
+		
+		System.out.println ( "-----------------------------" ) ;	
+		
+		
+//		單一檔案測試是否存在
 		checkFile c = new check_file_noloop ( Single_File ) ;
 		System.out.println ( c.get_noloop_boolean() ) ;
 //		c.get_all();		使用非file_noloop的Method會產生錯誤
 //		c.get_exist();		使用非file_noloop的Method會產生錯誤
 //		c.get_noexist();	使用非file_noloop的Method會產生錯誤
-//		c.get_boolean();	使用非file_noloop的Method會產生錯誤
+//		c.get_boolean();	使用非file_noloop的Method會產生錯誤		
+		
+		
+
+
 	}
 
 }

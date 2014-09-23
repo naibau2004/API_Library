c560e09098340a1253627143ef82b79ecbf296c4
Eric <naibau2004@gamil.com>
2014-09-23 21:50:24 +0800

    Develop 2014/09/23

--- API_Src/simpleMail/simpleMail.java
+++ API_Src/simpleMail/simpleMail.java
@@ -1,14 +1,17 @@
-package mail_Test;
+package simpleMail;
 
-// 需要下載「javamail」的API，將mail.jar匯入
 import java.util.Properties;
+
+//在程式撰寫之前要將「javamail」裡面的「mail.jar」放進lib裡面
+
+import javax.mail.* ;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class mail_Test 
+public class simpleMail 
 {
 	public static void main(String[] args)
 	{
@@ -17,15 +20,16 @@ public class mail_Test
 //			設定傳送基本資訊
 			String host = "172.16.200.27" ;
 			String from = "chichun_yu@syscom.com.tw" ;
-			String to = "naibau2004@gmail.com" ;
+			String to = "chichun_yu@syscom.com.tw" ;
 			
-//			這行不曉得幹啥用的
+			
+//			這行不曉得幹啥用的，這行連同下面二行為建立mail連線的標準程式碼
 			Properties props = System.getProperties() ;
 			
-//			設定SMTP Server，前面的"mail.smtp.host"固定照抄
+//			設定SMTP Server，前面的"mail.smtp.host"固定照抄，「host」是變數
 			props.put("mail.smtp.host", host ) ;
 			
-//			依據Properties建立一個session
+//			取得與SMTP Server的連線，props使用「props.put」取得mail Server
 			Session mailSession = Session.getDefaultInstance( props , null ) ;
 			
 //			把Session餵給Message(指定寄件mail Server，而Message用來建立信件)
@@ -38,13 +42,14 @@ public class mail_Test
 			MailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to) ) ;
 			
 //			設定郵件標題
-			MailMessage.setSubject( "Hello JavaMail" ) ;
+			MailMessage.setSubject( "Java測試信使用" ) ;
 			
 //			設定郵件內文
-			MailMessage.setText( "Wellcome to JavaMail...!" ) ;
+			MailMessage.setText( "這是一個測試信abc123!@#$%^&*" ) ;
 			
 //			傳送
 			Transport.send( MailMessage );
+			
 			
 //			成功後顯示傳送成功
 			System.out.println ("Mail was sent successfully !\n" ) ;
@@ -54,4 +59,5 @@ public class mail_Test
 			System.out.println ( e.getMessage() ) ;
 		}
 	}
+
 }

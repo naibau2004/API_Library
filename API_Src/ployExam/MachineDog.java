86a6ec2d14f992ab34ee48d47a2cec905689831c
Eric <naibau2004@gamil.com>
2015-05-10 11:45:24 +0800

    Commit 20150510

--- API_Src/ployExam/MachineDog.java
+++ API_Src/ployExam/MachineDog.java
@@ -3,14 +3,14 @@ package ployExam;
 public class MachineDog implements Machine {
 
 	@Override
+	public void bark() {
+		System.out.println("機機機機機");	
+	}
+	
+	@Override
 	public void light() {
 		System.out.println("嗡嗡嗡嗡嗡");
 		
-	}
-
-	@Override
-	public void bark() {
-		System.out.println("機機機機機");	
 	}
 	
 }
